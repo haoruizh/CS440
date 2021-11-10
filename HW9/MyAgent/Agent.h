cdc9db89fd8f707b1ed280a13bf5865f866c19f0
Haorui Zhang <haoruizh@gmail.com>
2021-11-09 22:29:56 -0800

    add hw5 sol and save my own agent in a folder

---
+++ HW9/MyAgent/Agent.h
@@ -0,0 +1,42 @@
+// Agent.h
+
+#ifndef AGENT_H
+#define AGENT_H
+
+#include "Action.h"
+#include "Percept.h"
+
+#include "Location.h"
+#include "Orientation.h"
+#include "Search.h"
+#include <list>
+
+class MySearchEngine: public SearchEngine {
+	virtual int HeuristicFunction (SearchState* state, SearchState* goalState);
+};
+
+class Agent
+{
+public:
+	Location goldLocation;
+	Location wumpusLocation;
+	vector<Location> stenches;
+	int X;
+	int Y;
+	Orientation orientation;
+	bool hasArrow;
+	bool hasGold;
+	Agent ();
+	~Agent ();
+	void Initialize ();
+	Action Process (Percept& percept);
+	void GameOver (int score);
+	void HandleAction(Action& action, Percept& percept);
+	void AddAdjacent();
+	list<Action> actionList;
+	MySearchEngine searchEngine;
+	list<Location> visited;
+	list<Location> safeLocations;
+};
+
+#endif // AGENT_H
